c866d27ed530590da0c83fa7e51da98a15fe7782
iCalculated <fufa0001@gmail.com>
2021-09-24 16:30:17 -0500

    events route

--- express_backend/src/app.ts
+++ express_backend/src/app.ts
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
   res.send('Hello Social Coding!')
 })
 
+app.get('/events', async (req, res) => {
+  const events = await prisma.event.findMany()
+  res.json(events)
+})
+
 app.listen(port, () => {
   console.log(`App listening at localhost:${port}`)
 })
