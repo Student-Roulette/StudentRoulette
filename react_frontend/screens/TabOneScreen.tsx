c201ccee77d0808a37866e6f62e7d6d85f2a38b9
iCalculated <fufa0001@gmail.com>
2021-09-24 17:52:07 -0500

    loading status

--- react_frontend/screens/TabOneScreen.tsx
+++ react_frontend/screens/TabOneScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, FlatList } from 'react-native';
+import { StyleSheet, FlatList, ActivityIndicator } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -13,18 +13,25 @@ interface EventProperties {
 
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [isLoading, setLoading] = useState(true)
   const [data, setData] = useState<EventProperties[]>([])
 
   const getEvents = async () => {
-    const response = await fetch('localhost:3000/events/', {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    })
-    const json = await response.json();
-    setData(json.events);
+    try {
+      const response = await fetch('localhost:3000/events/', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+      const json = await response.json();
+      setData(json.events);
+    } catch(error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
   };
 
   useEffect(() => {
@@ -34,14 +41,17 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}> {isLoading ? "loading" : "loaded"} </Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <FlatList
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
           data={data}
-          //keyExtractor={({ id }, index) => id}
+          keyExtractor={({id}, index) => id.toString()}
           renderItem={({ item }) => (
             <Text>{item.title}, {item.description}</Text>
           )}
-      />
+        />
+      )}
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
