578ae3cc208174d24e2be651a11829ddd7be4d5a
iCalculated <fufa0001@gmail.com>
2021-09-24 16:26:31 -0500

    prisma client in express

--- express_backend/src/app.ts
+++ express_backend/src/app.ts
@@ -1,6 +1,11 @@
-const express = require('express')
+import { Prisma, PrismaClient } from '@prisma/client'
+import express from 'express'
+
 const app = express()
+const prisma = new PrismaClient()
 const port = 3000
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello Social Coding!')
