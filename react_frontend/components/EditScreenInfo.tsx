cb2bf81a798c65fc305decc4739e704050ffb016
David Malenke <dnmalenke@gmail.com>
2021-09-30 18:48:50 -0500

    no.

--- react_frontend/components/EditScreenInfo.tsx
+++ react_frontend/components/EditScreenInfo.tsx
@@ -15,6 +15,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
           Open up the code for this screen:
+          NO.
         </Text>
 
         <View
