a247249f0389f2bc6f0a53009cb5ad88414f948a
Evan Voogd <voogd004@umn.edu>
2021-10-14 18:54:14 -0500

    Built classes for Tag and Attraction

--- express_backend/caching/cacher.ts
+++ express_backend/caching/cacher.ts
@@ -1,7 +1,24 @@
-// Data Structure 
+class Tag {
+    id: number;
+    createdAt: Date;
+    updatedAt: Date;
+    name: String;
+    parentID: number;
+}
+
+class Attraction {
+    id: number;
+    createdAt: Date;
+    updatedAt: Date;
+    name: String;
+    descripion: String;
+    startTime: Date;
+    endTime: Date;
+    tags: Tag[];
+}
 
 function pullFromMaps() {
-
+    
 }
 
 function pushToDB() {
