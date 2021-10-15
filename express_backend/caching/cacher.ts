7498c52e4b6fd1088dfc841e18e9236fa7c0f83d
minizou <xiaosuantang@gmail.com>
2021-10-15 00:28:00 +0000

    Created pushToDB and helpers.

--- express_backend/caching/cacher.ts
+++ express_backend/caching/cacher.ts
@@ -1,3 +1,5 @@
+import { PrismaClient, Prisma } from '@prisma/client'
+
 class Tag {
     id: number;
     createdAt: Date;
@@ -11,7 +13,7 @@ class Attraction {
     createdAt: Date;
     updatedAt: Date;
     name: String;
-    descripion: String;
+    description: String;
     startTime: Date;
     endTime: Date;
     tags: Tag[];
@@ -19,10 +21,55 @@ class Attraction {
 
 let attractionList: Array<Attraction>;
 
+function retrieveTag(tag : Tag) {
+    return {
+        id: tag.id,
+        createdAt: tag.createdAt,
+        updatedAt: tag.updatedAt,
+        name: tag.name,
+        parentID: tag.parentID,
+    }
+}
+
+function retrieveAttraction(attraction : Attraction) {
+	return {
+        id : attraction.id,
+        createdAt: attraction.createdAt,
+        updatedAt: attraction.updatedAt,
+		name: attraction.name,
+        description: attraction.description,
+		startTime: attraction.startTime,
+		endTime: attraction.endTime,
+        tags: attraction.tags,
+	}
+}
+
 function pullFromMaps() {
 
 }
 
-function pushToDB() {
-    
+async function pushToDB() {
+    console.log('Attempting to push to DB...')
+    const prisma = new PrismaClient()
+
+    /*
+    console.log(`Pushing tags to DB...`)
+	for (let i = 0; i < tagList.length; i++) {
+		const tag = await prisma.tag.create({
+		    data: retrieveTag(tagList[i]),
+		})
+		console.log(`Created tag with id: ${tag.id}`)
+	}
+    console.log(`Pushed tags to DB.`)
+    */
+
+    console.log(`Pushing attractions to DB...`)
+    for (let i = 0; i < attractionList.length; i++){
+        const attraction = await prisma.attraction.create({
+            data: retrieveAttraction(attractionList[i]),
+		})
+        console.log(`Pushed attraction to DB with id: ${attraction.id}`)
+    }
+    console.log(`Pushed attractions to DB.`)
+    console.log(`Pushing to DB finished.`)
 }
