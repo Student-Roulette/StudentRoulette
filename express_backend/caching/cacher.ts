c298705dbc85570ef29d35ab3a02d6015c183cc5
Evan Voogd <voogd004@umn.edu>
2021-10-21 18:50:47 -0500

    pullFromMaps implementation that will never be used lol.

--- express_backend/caching/cacher.ts
+++ express_backend/caching/cacher.ts
@@ -1,3 +1,5 @@
+import { ServerResponse } from "http";
+
 class Tag {
     id: number;
     createdAt: Date;
@@ -11,7 +13,7 @@ class Attraction {
     createdAt: Date;
     updatedAt: Date;
     name: String;
-    descripion: String;
+    description: String;
     startTime: Date;
     endTime: Date;
     tags: Tag[];
@@ -19,8 +21,45 @@ class Attraction {
 
 let attractionList: Array<Attraction>;
 
-function pullFromMaps() {
+const apiKey = "AIzaSyC0nM4JMB8ZKRtiUe1dtraibHq38k9EajM";
 
+let id = 0;
+
+function pullFromMaps() {
+    var types = ["amusement_park", "aquarium", "art_gallery", "bakery", "bar", "beauty_salon", "bowling_alley", "cafe", "campground", "casino", "church", "gym", "hair_care",
+                "hindu_temple", "library", "mosque", "movie_theater", "museum", "night_club", "park", "restaurant", "shopping_mall", "spa", "stadium", "store", "synagogue",
+                "tourist_attraction", "zoo"];
+
+    var axios = require('axios');    
+
+    for (let i: 0; i < types.length; i++) {
+        let apiURL: string =  "https://maps.googleapis.com/maps/api/place/nearbysearch/json" +
+                              "?location=44.973925%2C-93.233133"
+                              "&key=" + apiKey +
+                              "&language=en" +
+                              "&radius=2415" +
+                              "&type=" + types[i];
+        var config = {
+            method: 'get',
+            url: apiURL,
+            headers: { }
+        };
+
+        axios(config)
+        .then(function (response) {
+            for (var i = 0; i < response.data.results.length; i++) {
+                var obj = response.data.results[i];
+                var attract = new Attraction();
+
+                attract.name = obj.name;
+                //add hours lol
+                attractionList.push(attract);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });    
+    }
 }
 
 function pushToDB() {
