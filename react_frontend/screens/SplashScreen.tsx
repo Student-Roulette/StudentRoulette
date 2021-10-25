cca1bd4f140fe695227753d173d8b8c79dd16d83
Kanishk-K <kanishkkacholia@gmail.com>
2021-10-25 23:13:45 +0000

    Added trending bar using dummy generation. ToDo: Connect with database.

--- react_frontend/screens/SplashScreen.tsx
+++ react_frontend/screens/SplashScreen.tsx
@@ -1,8 +1,24 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, FlatList, Animated } from "react-native";
 import MaterialButton from "../components/MaterialButton";
+import MaterialCardWithImageAndTitle from "../components/MaterialCardWithImageAndTitle";
 
 function SplashScreen(props: any) {
+  let TEMP_DATA: Attraction[] = [];
+  for (var i = 0;i<10;i++){
+    var time: Date = new Date();
+    TEMP_DATA.push({id:i, createdAt:time.toString(),updatedAt:time.toString(), name:`Thing_${i}`, startTime:`${i}`, endTime:`${i+1}`});
+  }
+  const renderCard = ({item} : {item : Attraction}) => (
+    <MaterialCardWithImageAndTitle
+      key={item.id}
+      title={item.name}
+      startTime={new Date(item.startTime)}
+      endTime={new Date(item.endTime)}
+      style={styles.materialCardWithImageAndTitle}
+    />
+  );
+
   return (    
     <View>
       <Text style={styles.welcome}>Welcome!</Text>
@@ -13,6 +29,17 @@ function SplashScreen(props: any) {
         text="SEARCH"
         icon="shopping-search"
       /> 
+      <Text>Trending/Upcoming</Text>
+      <FlatList 
+        horizontal={true} 
+        data={TEMP_DATA} 
+        renderItem={renderCard}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        snapToAlignment="start"
+        decelerationRate="fast"
+        >
+      </FlatList>
     </View>
   );
 }
@@ -32,6 +59,13 @@ const styles = StyleSheet.create({
   },
   searchButtonTextStyle:{
     fontSize:24
+  },
+  materialCardWithImageAndTitle: {
+    height: 166,
+    width: 325,
+    marginTop: 27,
+    marginHorizontal: 17,
+    alignSelf: "center"
   }
 });
 
