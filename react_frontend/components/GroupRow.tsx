b7fe0cc554c5bc1e55e43188d9676d96698439e3
Kanishk-K <kanishkkacholia@gmail.com>
2021-12-12 18:02:53 +0000

    Created cards that display groups, recolor and modify css as per stylesheet.

--- react_frontend/components/GroupRow.tsx
+++ react_frontend/components/GroupRow.tsx
@@ -1,34 +1,49 @@
 import React, { Component } from "react";
 import { StyleSheet, View, TouchableOpacity, Text, Image } from "react-native";
+import { Card, Avatar, Badge } from "react-native-paper";
+
+/*
+ 
+
+*/
 
 function GroupRow(props: any) {
   return (
-    <View style={styles.container}>
-    <Image
-        source={require("../assets/images/cardImage.png")}
-        style={styles.GroupIMG}
-    ></Image>
-      <Text style={styles.GroupTitle}>{props.Name}</Text>
-    </View>
+    <Card style = {[styles.cardStyle,styles.cardShadow]}>
+      {props.UpcomingEvent ? 
+      <Card.Title 
+      title={props.Name}
+      subtitle={"{Insert Group Leadership Role Here}"}
+      left={(props) => <Avatar.Image size={70} source={require("../assets/images/cardImage.png")}/>}
+      right={(props) => <Badge size={30}>!</Badge>}
+      leftStyle={styles.left}
+      />
+      :
+      <Card.Title 
+      title={props.Name}
+      subtitle={"{Insert Group Leadership Role Here}"}
+      left={(props) => <Avatar.Image size={70} source={require("../assets/images/cardImage.png")}/>}
+      leftStyle={styles.left}
+      />}
+    </Card> 
   );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flexDirection: "row",
-        alignItems:"center",
-        height: 60,
-        margin: 10
-    },
-    GroupIMG:{
-        height:60,
-        width:60,
-        borderRadius:60/2
-    },
-    GroupTitle:{
-        fontSize:20,
-        marginLeft:30
-    }
+  cardStyle:{
+    padding:10, 
+    margin:10, 
+    backgroundColor:"#eddfdf"
+  },
+  cardShadow:{
+    shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3
+  },
+  left:{
+    marginRight:35
+  }
 });
 
 export default GroupRow;
--- react_frontend/screens/MyGroups.tsx
+++ react_frontend/screens/MyGroups.tsx
@@ -1,17 +1,62 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
 import MaterialButton from "../components/MaterialButton";
+import GroupRow from "../components/GroupRow";
 
 function MyGroups(props: any) {
+  const TEMP_DATA = [
+    {
+      key:"1",
+      Name:"Group Numero Uno",
+      UpcomingEvent:true
+    },
+    {
+      key:"2",
+      Name:"Group Numero Dos",
+      UpcomingEvent:false
+    },
+    {
+      key:"3",
+      Name:"Group Numero Tres",
+      UpcomingEvent:false
+    },
+    {
+      key:"4",
+      Name:"Group Numero Quatro",
+      UpcomingEvent:true
+    },
+    {
+      key:"5",
+      Name:"Group Numero Cinco",
+      UpcomingEvent:false
+    },
+    {
+      key:"6",
+      Name:"Group Numero Seis",
+      UpcomingEvent:false
+    },
+    {
+      key:"7",
+      Name:"Group Numero Siete",
+      UpcomingEvent:true
+    },
+  ]
   return (    
-    <View>
-      <Text>My Groups</Text>
-    </View>
+    <FlatList
+    style={styles.list}
+    data={TEMP_DATA}
+    renderItem={({item}) => GroupRow(item)}
+    showsVerticalScrollIndicator={false}
+    showsHorizontalScrollIndicator={false}
+    >  
+    </FlatList>
   );
 }
 
 const styles = StyleSheet.create({
-
+  list:{
+    paddingBottom:70
+  }
 });
 
 export default MyGroups;
