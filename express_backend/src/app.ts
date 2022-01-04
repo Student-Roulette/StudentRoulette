5336b95b17265749b64be7b6edeb3ca8ed01b2ea
minizou <xiaosuantang@gmail.com>
2022-01-04 14:41:08 -0600

    Add API routes (CRUD) for tags/groups/users/events
    Changed :id to :name for some routes depending on
    accessibility (example: tags are accessed by name rather
    than id).

--- express_backend/src/app.ts
+++ express_backend/src/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 import cors from "cors";
@@ -148,13 +149,13 @@ app.get("/event/:id", async (req, res) => {
   res.json(event);
 });
 
-// GET /tags/list
+// tags
+
 app.get(`/tags/list`, async (req, res) => {
   const tags = await prisma.tag.findMany();
   res.json(tags);
 });
 
-// GET tags/relations
 app.get("/tags/relations/:name", async (req, res) => {
   const { name } = req.params;
   const relations = await prisma.tag.findUnique({
@@ -169,9 +170,20 @@ app.get("/tags/relations/:name", async (req, res) => {
   res.json(relations);
 });
 
-// TODO - POST /tags/new
+// FIXME - check children+attractions, check unique name
+app.post("/tags/new", async (req, res) => {
+  console.log(req.body);
+  const { name, children, attractions} = req.body;
+  const result = await prisma.tag.create({
+    data: {
+      name,
+      children,
+      attractions
+    },
+  });
+  res.json(result);
+});
 
-// DEL /event/:id
 app.delete(`/event/:id`, async (req, res) => {
   const { id } = req.params;
   const event = await prisma.attraction.delete({
@@ -187,7 +199,7 @@ app.delete(`/event/:id`, async (req, res) => {
 // TODO GET /event/tag/:tag
 // TODO PUT /event/:id
 
-// DEL /groups/:id
+// groups
 app.delete(`/groups/:id`, async (req, res) => {
   const { id } = req.params;
   const event = await prisma.group.delete({
@@ -197,6 +209,160 @@ app.delete(`/groups/:id`, async (req, res) => {
   });
   res.json(event);
 });
+
+app.get("/groups/members/:id", async (req, res) => {
+  const { id } = req.params;
+  const members = await prisma.group.findUnique({
+    where: {
+      id: Number(id),
+    },
+    select: {
+      members: true,
+    },
+  });
+  res.json(members);
+});
+
+// TODO - GET /groups/logo/:id
+// either [1] add logo field to schema or [2] write a helper function
+
+app.get("/groups/verified/:id", async (req, res) => {
+  const { id } = req.params;
+  const verified = await prisma.group.findUnique({
+    where: {
+      id: Number(id),
+    },
+    select: {
+      verified: true,
+    },
+  });
+  res.json(verified);
+});
+
+app.post("/groups/new", async (req, res) => {
+  console.log(req.body);
+  const { name, description, verified, creatorId } = req.body;
+  const result = await prisma.group.create({
+    data: {
+      name,
+      description,
+      verified: false || verified,
+      creatorId,
+    },
+  });
+  res.json(result);
+});
+
+// TODO - PUT /groups/update/:id
+app.put("/groups/update/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, description, verified, members, attractions } = req.body;
+  const group = await prisma.group.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      name,
+      description,
+      verified,
+      members,
+      attractions,
+    },
+  });
+  res.json(group);
+});
+
+// FIXME - PUT /groups/join/:id
+/*
+app.put("/groups/join/:id", async (req, res) => {
+  const { id } = req.params;
+  const { user } = req.body;
+
+  const addUser = await prisma.group.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      members: {
+        push: user,
+      },
+    },
+  });
+  res.json(addUser);
+});
+*/
+
+// users
+
+app.get("/users/banned/:id", async (req, res) => {
+  const { id } = req.params;
+  const banned = await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+    select: {
+      banned: true,
+    },
+  });
+  res.json(banned);
+});
+
+app.get("/users/groups/:id", async (req, res) => {
+  const { id } = req.params;
+  const groups = await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+    select: {
+      groups: true,
+    },
+  });
+  res.json(groups);
+});
+
+// FIXME - intended to be used by admin - any differences to writing this?
+// (security concerns? extra restrictions?)
+app.get("/users/events/:id", async (req, res) => {
+  const { id } = req.params;
+  const events = await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+    select: {
+      events_created: true,
+    },
+  });
+  res.json(events);
+});
+
+// FIXME - see above
+app.get("/users/founded/:id", async (req, res) => {
+  const { id } = req.params;
+  const groups = await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+    select: {
+      groups_created: true,
+    },
+  });
+  res.json(groups);
+});
+
+app.post("/users/signup", async (req, res) => {
+  console.log(req.body);
+  const { name, email } = req.body;
+  const result = await prisma.user.create({
+    data: {
+      name,
+      email,
+      banned: false,
+    },
+  });
+  res.json(result);
+});
+
+// TODO - /auth/ routing ??
 
 app.listen(port, () => {
   console.log(`App listening at localhost:${port}`);
